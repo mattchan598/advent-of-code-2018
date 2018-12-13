aca3e313f50568fcd7a3d21404ddcc989c6c5b8b
Matt Chan <chan.598@osu.edu>
2018-12-13 02:10:47 -0500

    Solved 2.1

--- Day 2.1.py
+++ Day 2.1.py
@@ -12,3 +12,17 @@ except IOError:
 
 # Create a dictionary of lowercase letters
 d = dict.fromkeys(string.ascii_lowercase, 0)
+num_twos = 0
+num_threes = 0
+
+for line in lines:
+    for letter in line.strip():
+        d[letter] += 1
+    if 2 in d.values():
+        num_twos += 1
+    if 3 in d.values():
+        num_threes += 1
+    d = dict.fromkeys(string.ascii_lowercase, 0)
+print(num_twos)
+print(num_threes)
+print(num_twos*num_threes)
