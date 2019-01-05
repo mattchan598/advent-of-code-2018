699e53c55c8fdeadf6c9ba5964f3c29cb1f2488c
Matt Chan <chan.598@osu.edu>
2019-01-05 11:28:33 -0500

    Added function to determine polarity

--- Day 5.1.py
+++ Day 5.1.py
@@ -1,5 +1,8 @@
+# function that determines if two units are the opposite polarity (two letters are opposite case)
+def isOppositePolarity(unit1, unit2):
+    return abs(ord(unit1) - ord(unit2)) == 32
+
 # Get input
 with open("Day_5_input.txt", "r") as myfile:
     polymer = myfile.read()
 
-print(polymer)
