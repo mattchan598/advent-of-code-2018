44450dcbac76a5c9e3056eaa6d2cd5f2ec3a6c43
Matt Chan <chan.598@osu.edu>
2019-01-06 17:27:56 -0500

    Finished 5.1

--- Day 5.1.py
+++ Day 5.1.py
@@ -1,3 +1,4 @@
+import string
 # function that determines if two units are the opposite polarity (two letters are opposite case)
 def isOppositePolarity(unit1, unit2):
     return abs(ord(unit1) - ord(unit2)) == 32
@@ -6,3 +7,14 @@ def isOppositePolarity(unit1, unit2):
 with open("Day_5_input.txt", "r") as myfile:
     polymer = myfile.read()
 
+copyString = ""
+
+while copyString != polymer:
+    copyString = polymer
+    for char in string.ascii_lowercase:
+        lowerUpperPair = char + char.upper()
+        upperLowerPair = char.upper() + char
+        polymer = polymer.replace(lowerUpperPair, "")
+        polymer = polymer.replace(upperLowerPair, "")
+
+print(len(polymer))
